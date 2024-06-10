1b9500bf13d612aec1471e6d943aa5c2f0798a17
Vitor Moraes - TI - IPOG Goiânia <vitor.moraes@ipog.local>
2024-06-10 18:10:04 -0300

    Codigos html e css mais concisos

--- agenda-app/src/app/app.component.ts
+++ agenda-app/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'agenda-app';
+export class AppComponent implements OnInit {
+  adicionar: boolean = true;
+  editar: boolean = false;
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit() {
+    this.adicionar = false
+    this.editar = false
+  }
+    
 }
